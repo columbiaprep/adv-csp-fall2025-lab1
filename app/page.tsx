86240abbb8efed816dd7e2409beddf46a32e6d0f
DACGPS <dabarshalin28@cgps.org>
2025-09-22 09:06:40 -0400

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import ShinyButton from "./components/sample-component";
 import { Avatar } from "@heroui/react";
+import ModalComponent from "./components/modal";
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-              <ShinyButton />
-              <h1 className="bg-sky-900 text-green-800 border-red-500 border-5">Hey</h1>
-
+    <div className="font-sans justify-center items-center">
+            
+              <ModalComponent />
     </div>
   );
 }
