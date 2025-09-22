5c63c7278eb38c0e6bee89fb98cf82eece9b4af4
Ryder Lawrence <ryderlawrence@ryders-mbp.cgps.domain>
2025-09-22 08:45:25 -0400

    First commit

---
+++ app/components/avatar.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Button } from "@heroui/react";
+import {Avatar, AvatarGroup, AvatarIcon} from "@heroui/avatar";
+
+export default function RyderAvatar() {
+    return (
+        <>
+        
+        </>
+    )
+}
