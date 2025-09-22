749a7638c35a18804d10fab12d641d67ebc47f7c
Jake Weiner <jakeweiner@Jakes-MacBook-Air-2.local>
2025-09-22 09:10:10 -0400

    First

--- app/components/input-action.tsx
+++ app/components/input-action.tsx
@@ -3,16 +3,22 @@ import {Input, Button} from "@heroui/react";
 
 export default function InputAction() {
   return (
-    <>
-    <Input
-      isReadOnly
-      className="max-w-xs"
-      defaultValue="junior@heroui.com"
-      label="Email"
-      type="email"
-      variant="bordered"
-    />
-    <Button color="primary">Button</Button>
-    </>
+    <div className="flex items-end gap-8">
+      <Input
+        isReadOnly
+        className="max-w-xs font-sans border border-slate-300 focus:border-blue-800"
+        defaultValue="jweiner27@cgps.org"
+        label="Email"
+        type="email"
+        variant="bordered"
+        placeholder="Enter your email"
+      />
+      <Button
+        color="primary"
+        className="bg-blue-800 text-white font-sans"
+      >
+        Button
+      </Button>
+    </div>
   );
 }
