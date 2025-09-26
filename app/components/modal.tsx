93fa44d26c35757583a0d70e217ff3be7c8f2247
DACGPS <dabarshalin28@cgps.org>
2025-09-26 10:30:54 -0400

    formating

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -22,7 +22,6 @@ export default function ModalComponent() {
           base: "border-[#292f46] bg-[#19172c] dark:bg-blue-800 text-[#000000] max-w-4xl",
           header: "border-b-[1px] border-[#292f46] flex flex-row justify-between items-center",
           footer: "border-t-[1px] border-[#292f46]",
-          
         }}
         >
         <ModalContent className="rounded-xl justify-center items-center">
@@ -43,12 +42,14 @@ export default function ModalComponent() {
                   className="rounded-lg bg-slate-100 text-blue-600 hover:bg-slate-200 font-sans px-2" 
                   variant="flat" 
                   onPress={onClose}
-                >Close
+                >
+                  Close
                 </Button>
                 <Button 
                   className="bg-green-500 text-white rounded-lg font-sans px-2" 
                   variant="flat" 
-                  onPress={onClose}>Thingy
+                  onPress={onClose}>
+                    Thingy
                 </Button>
               </ModalFooter>
             </>
