63467fdf6b61282c7c084b0209fd31890d66ad41
Ryder Lawrence <ryderlawrence@ryders-mbp.cgps.domain>
2025-09-30 12:10:49 -0400

    Lab 3

--- app/components/avatar.tsx
+++ app/components/avatar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React from "react";
 import { Button } from "@heroui/react";
 import {Avatar, AvatarGroup, AvatarIcon} from "@heroui/avatar";
 
@@ -26,4 +27,32 @@ export default function RyderAvatar() {
       </div>
     </div>
   );
-}
+};
+  interface RyderAvatarProps{
+    name: string;
+    status?: string;
+    bgColor?: string;
+  }
+  export const RyderAvatarWithProps: React.FC<RyderAvatarProps> = ({
+  name,
+  status = "Online",
+  bgColor = "bg-blue-800",
+}) => {
+  return (
+    <div className="flex items-center gap-4 p-4 bg-slate-100 rounded-2xl shadow">
+      <div className="relative">
+        <Avatar
+          name={name}
+          className={`w-16 h-16 font-sans ${bgColor} text-white`}
+          />
+          <span className ="absolute bottom-1 right-1 block w-4 h-4 rounded-full bg-emerald-500 border-2 border-white" />
+      </div> 
+
+      <div>
+         <p className="font-sans text-slate-800 text-lg font-semibold">{name}</p>
+        <p className="font-sans text-slate-800 text-sm">{status}</p>
+      </div>
+
+      </div>
+  );
+};
