52b30d4cefb0b1eb7384000b1865242eba8aa827
Armen Azadian <armenazadian@Armens-MacBook-Air-5.local>
2025-09-30 12:10:56 -0400

    lab 3 but no progress bar

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import ShinyButton from "./components/sample-component";
 import { Avatar } from "@heroui/react";
 import ModalComponent from "./components/modal";
+import ArmenBox from "./components/progress-component";
 export default function Home() {
   return (
     <div className="font-sans justify-center items-center">
     
-              <ModalComponent />
+              <ArmenBox value={42} label="Progress Bar" cardText="Almost There!" />
     </div>
   );
 }
